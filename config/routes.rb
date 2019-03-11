d3f4da8d69fbea3a48cde19d4018ca7a054675f0
hiroki-okazaki <1031okazaki@gmail.com>
2019-03-12 07:26:19 +0900

    bundle install実行漏れ

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # URLは/calcs/までは固定。それ以降をパラメータで受け取り
+  get 'calcs/:value1/:operator/:value2' => 'calcs#show'
 end
