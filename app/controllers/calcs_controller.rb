4a9927d628879fe18b46e91b5777a157c36a5a18
hiroki-okazaki <1031okazaki@gmail.com>
2019-03-13 21:35:32 +0900

    原因不明なるもバグ解消

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -1,11 +1,11 @@
 class CalcsController < ApplicationController
-　
+
   def show
     value1 = params[:value1].to_i
     value2 = params[:value2].to_i
     operator = params[:operator]
-    
-  @computation = case operator
+
+    @computation = case operator
       when 'addition'
         value1 + value2
       when 'subtraction'
@@ -13,7 +13,7 @@ class CalcsController < ApplicationController
       when 'multiplication'
         value1 * value2
       when 'division'
-        value1 / value2
+        value1 / value2.to_f
       end
   end
 end
